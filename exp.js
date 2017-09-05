f7f9605ae06b5cf1ed624cbc957ced70f19e539d
arjunjain30 <arjunjain1996@gmail.com>
2017-09-05 16:58:30 +0530

    Add files via upload

---
+++ exp.js
@@ -0,0 +1,11 @@
+var $navList = $('.nav-list');
+
+$navList.on('click', 'li:not(.selected)', function(e){
+  $navList.find(".selected").removeClass("selected");
+  $(e.currentTarget).addClass("selected");
+});
+
+
+
+
+
